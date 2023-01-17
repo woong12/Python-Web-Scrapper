c3e5133bb3710f448a01752a613eeb13f352c63c
woong <koohw0109@gmail.com>
2023-01-17 20:27:32 +0900

    requests

--- Data Structures/for.py
+++ Data Structures/for.py
@@ -1,3 +1,5 @@
+from requests import get 
+
 websites = (
     "https://google.com",
     "airbnb.com",
@@ -6,7 +8,15 @@ websites = (
     "https://tictok.com",
 )
 
+results = {}
+
 for website in websites:
     if not website.startswith("https://"):
         website = f"https://{website}"
-    print(website)
+    response = get(website)
+    if response.status_code == 200:
+        results[website] = 'OK'
+    else:
+        results[website] = 'FAILED'
+
+print(results)
