db99c4f40e5f956610f928ea68e8b914686de1c0
woong <koohw0109@gmail.com>
2023-01-20 23:14:17 +0900

    comment

--- JOB SCRAPPER/scrapper.py
+++ JOB SCRAPPER/scrapper.py
@@ -1,21 +1,32 @@
-from requests import get
-from bs4 import BeautifulSoup
+# 1.요청
+# 2.응답
+# 3.응답 파싱
+# 4.데이터를 원하는대로 만지기
 
+# 다운로드한 library 포함
+from requests import get # http 요청을 위해 lib
+from bs4 import BeautifulSoup # 응답 결과를 파싱 하여 가공하기 위한 lib
+
+# 요청 사이트 url
 main_url = "https://weworkremotely.com/remote-jobs/search?term="
-
+# 검색할 키워드
 search_term = "python"
 
+# 요청후 응답 받기
 response = get(f"{main_url}{search_term}")
 
+# 응답에 따른 분기문
 if not response.status_code == 200:
     print("Can't request the website!")
 else:
-    results = []
+    # html을 파싱하여 결과 받기
     soup = BeautifulSoup(response.text ,"html.parser")
+    # 결과에서 원하는 데이터 추출
+    results = []
     jobs = soup.find_all('section', class_="jobs")
     for job_section in jobs:
         job_posts = job_section.find_all('li')
-        job_posts.pop(-1)
+        job_posts.pop(-1) # 마지막 요소 제거
         for post in job_posts:
             anchors = post.find_all('a')
             anchor = anchors[1]
