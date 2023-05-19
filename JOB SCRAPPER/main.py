2f3306d63efa524f1d1b1a5fc73f4954a1a8df4e
woong <koohw0109@gmail.com>
2023-05-19 22:15:44 +0900

    render templates

--- JOB SCRAPPER/main.py
+++ JOB SCRAPPER/main.py
@@ -1,17 +1,37 @@
-from extractors.indeed import extract_indeed_jobs
-from extractors.wwr import extract_wwr_jobs
+from flask import Flask, render_template
 
-keyword = input("What do you wnat to search for?")
+app = Flask("JobScrapper")
 
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-jobs = indeed + wwr
 
-file = open(f"{keyword}.csv", "w", encoding="utf-8")
-file.write("Position,Company,Location,URL\n")
+@app.route("/")
+def home():
+    return render_template("home.html", name="abcd")
 
-for job in jobs:
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
 
-file.close()
-# aa
+@app.route("/hello")
+def hello():
+    return "hello you!"
+
+
+app.run("0.0.0.0")
+
+
+# from extractors.indeed import extract_indeed_jobs
+# from extractors.wwr import extract_wwr_jobs
+# from file import save_to_file
+
+# keyword = input("What do you wnat to search for?")
+
+# indeed = extract_indeed_jobs(keyword)
+# wwr = extract_wwr_jobs(keyword)
+# jobs = indeed + wwr
+# save_to_file(keyword, jobs)
+
+
+# file = open(f"{keyword}.csv", "w", encoding="utf-8")
+# file.write("Position,Company,Location,URL\n")
+
+# for job in jobs:
+#     file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
+
+# file.close()
