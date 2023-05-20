355b689435d84d1ad80334c56f22439a380cd15c
woong <koohw0109@gmail.com>
2023-05-20 15:19:05 +0900

    done

--- JOB SCRAPPER/extractors/indeed.py
+++ JOB SCRAPPER/extractors/indeed.py
@@ -3,25 +3,27 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 browser = webdriver.Chrome(options=options)
 
+
 def get_page_count(keyword):
-    base_url = 'https://kr.indeed.com/jobs?q='
+    base_url = "https://kr.indeed.com/jobs?q="
     browser.get(f"{base_url}{keyword}")
 
     soup = BeautifulSoup(browser.page_source, "html.parser")
     pagination = soup.find("nav", class_="ecydgvn0")
     if pagination == None:
         return 1
-    pages = pagination.find_all("div", recursive = False)
+    pages = pagination.find_all("div", recursive=False)
     count = len(pages)
     if count >= 5:
         return 5
     else:
         return count
+
 
 def extract_indeed_jobs(keyword):
     pages = get_page_count(keyword)
@@ -34,26 +36,27 @@ def extract_indeed_jobs(keyword):
         print("Requesting", final_url)
 
         soup = BeautifulSoup(browser.page_source, "html.parser")
-        job_list = soup.find('ul', class_= "jobsearch-ResultsList css-0")
-        jobs = job_list.find_all('li', recursive = False)
-        #ul 바로밑 li만을 찾아낼 것이다
+        job_list = soup.find("ul", class_="jobsearch-ResultsList css-0")
+        jobs = job_list.find_all("li", recursive=False)
+        # ul 바로밑 li만을 찾아낼 것이다
         for job in jobs:
             zone = job.find("div", class_="mosaic-zone")
-            #find는 찾은 element를 주거나 None을 준다
-            if zone == None:#job li에서 job을 추출한다
+            # find는 찾은 element를 주거나 None을 준다
+            if zone == None:  # job li에서 job을 추출한다
                 anchor = job.select_one("h2 a")
-                title = anchor['aria-label']
-                link = anchor['href']
-                company = job.find("span",class_="companyName")
-                region = job.find("div",class_="companyLocation")
+                title = anchor["aria-label"]
+                link = anchor["href"]
+                company = job.find("span", class_="companyName")
+                region = job.find("div", class_="companyLocation")
                 job_data = {
-                    'link' : f"https://www.indeed.com{link}",
-                    'company' : company.string.replace(",", " "),
-                    'location' : region.string.replace(",", " "),
-                    'position' : title.replace(",", " ")
+                    "link": f"https://www.indeed.com{link}",
+                    "company": company.string.replace(",", " "),
+                    "location": region.string.replace(",", " "),
+                    "position": title.replace(",", " "),
                 }
                 results.append(job_data)
     return results
+
 
 # jobs = extract_indeed_jobs("python")
 # print(len(jobs))
