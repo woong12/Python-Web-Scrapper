7eaa34f6a7eeb7a89f853518c350f129bc3d5088
woong <koohw0109@gmail.com>
2023-05-20 13:08:37 +0900

    Arguments

--- JOB SCRAPPER/main.py
+++ JOB SCRAPPER/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
 
@@ -10,7 +12,11 @@ def home():
 
 @app.route("/search")
 def hello():
-    return render_template("search.html")
+    keyword = request.args.get("keyword")
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 
 app.run("0.0.0.0")
