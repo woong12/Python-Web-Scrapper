48115fe1f4ac789ec7efa9828bc19aef4a8ede5d
woong <koohw0109@gmail.com>
2023-05-20 14:54:38 +0900

    file download

--- JOB SCRAPPER/main.py
+++ JOB SCRAPPER/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
 
@@ -18,8 +19,10 @@ db = {}
 
 
 @app.route("/search")
-def hello():
+def search():
     keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
     if keyword in db:
         jobs = db[keyword]
     else:
@@ -28,6 +31,17 @@ def hello():
         jobs = indeed + wwr
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.args("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/serch?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run("0.0.0.0")
